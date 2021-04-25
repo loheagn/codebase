0ce23b9e31fea305ba335cb398d3beec2b7a9286
loheagn <loheagn@icloud.com>
2021-04-25 15:06:22 +0800

    add test

--- go/kube/pod_test.go
+++ go/kube/pod_test.go
@@ -31,9 +31,8 @@ func Test_listPod(t *testing.T) {
 		name string
 		args args
 	}{
-		{args: args{"0-1", map[string]string{
-			"commit": "9a50aa80144d91f6a1587c8987aeb69d6a3f7cd4",
-		}}},
+		{args: args{"0-1", map[string]string{"commit": "9a50aa80144d91f6a1587c8987aeb69d6a3f7cd4"}}},
+		{args: args{"0-1", map[string]string{"commit": "hahhahahahah"}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
