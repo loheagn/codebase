e1efce0ea13f319507f957cad9a8e6c4ba3851d8
loheagn <loheagn@icloud.com>
2021-08-23 23:45:34 +0800

    minigrep: Creating a Constructor for Config

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -20,8 +20,10 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
