b7d90f2acce15e5e2e90025d1ee27f0d5ac76370
loheagn <loheagn@icloud.com>
2021-08-24 00:32:55 +0800

    minigrep: Splitting Code into a Library Crate

---
+++ rust/minigrep/src/lib.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("no enough arguments");
+        }
+        return Ok(Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        });
+    }
+}
--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,33 +14,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(err) = run(config) {
+    if let Err(err) = minigrep::run(config) {
         println!("Application error: {}", err);
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("no enough arguments");
-        }
-        return Ok(Config {
-            query: args[1].clone(),
-            filename: args[2].clone(),
-        });
-    }
 }
