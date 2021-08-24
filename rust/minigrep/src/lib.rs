3bc0b0fcbabba0df5f772eefa7c86631b2b710d5
loheagn <loheagn@icloud.com>
2021-08-24 20:29:35 +0800

    minigrep: remove `println!`

--- rust/minigrep/src/lib.rs
+++ rust/minigrep/src/lib.rs
@@ -4,8 +4,6 @@ use std::fs;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
-
     Ok(())
 }
 
--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -11,9 +11,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(err) = minigrep::run(config) {
         println!("Application error: {}", err);
         process::exit(1);
