d42579f315a58485af71bbade67b99776db75f44
loheagn <loheagn@icloud.com>
2021-08-28 17:19:09 +0800

    minigrep: Writing a Failing Test for the Case-Insensitive search Function

--- rust/minigrep/src/lib.rs
+++ rust/minigrep/src/lib.rs
@@ -45,12 +45,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["Rust:"], search_case_insensitive(query, contents))
     }
 }
