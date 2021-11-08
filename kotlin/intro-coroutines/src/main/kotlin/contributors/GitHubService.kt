b2475c7c7eb4d4fe359448420de915eff99831d2
loheagn <loheagn@icloud.com>
2021-11-08 23:39:20 +0800

    阅读到 concurrency

--- kotlin/intro-coroutines/src/main/kotlin/contributors/GitHubService.kt
+++ kotlin/intro-coroutines/src/main/kotlin/contributors/GitHubService.kt
@@ -16,15 +16,26 @@ import java.util.Base64
 
 interface GitHubService {
     @GET("orgs/{org}/repos?per_page=100")
-    fun getOrgReposCall(
+     fun getOrgReposCall(
         @Path("org") org: String
     ): Call<List<Repo>>
 
     @GET("repos/{owner}/{repo}/contributors?per_page=100")
-    fun getRepoContributorsCall(
+     fun getRepoContributorsCall(
         @Path("owner") owner: String,
         @Path("repo") repo: String
     ): Call<List<User>>
+
+    @GET("orgs/{org}/repos?per_page=100")
+    suspend fun getOrgRepos(
+        @Path("org") org: String
+    ): Response<List<Repo>>
+
+    @GET("repos/{owner}/{repo}/contributors?per_page=100")
+    suspend fun getRepoContributors(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): Response<List<User>>
 }
 
 @Serializable
