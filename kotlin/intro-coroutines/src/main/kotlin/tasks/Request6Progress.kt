096d33f83d52fda20c4421a72dd63ca8ccb67964
loheagn <loheagn@icloud.com>
2021-11-09 14:43:31 +0800

    Showing progress

--- kotlin/intro-coroutines/src/main/kotlin/tasks/Request6Progress.kt
+++ kotlin/intro-coroutines/src/main/kotlin/tasks/Request6Progress.kt
@@ -7,5 +7,21 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+
+    var users = emptyList<User>()
+    repos.forEachIndexed { index, repo ->
+        service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+            .also {
+                users = (users + it).aggregate()
+                updateResults(users, index == repos.lastIndex)
+            }
+    }
 }
